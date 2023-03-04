c00f763f0e9353cac555737f0be6af60d3a40941
moe <ali.jajeh@gmail.com>
2023-03-04 15:07:49 -0500

    Updated to support different operations

--- main.py
+++ main.py
@@ -5,21 +5,42 @@ import websockets
 import json
 import os 
 
-async def echo(websocket, path):
+def _move_device(options):
+    try:
+        device = options['device']
+        pan = options['pan']
+        tilt = options['tilt']
+        zoom = options['zoom']
+    except KeyError as e:
+        return "Missing data"
+
+    command = f"v4l2-ctl -c pan_absolute={pan},tilt_absolute={tilt},zoom_absolute={zoom} -d {device}"
+    return os.system(command)
+
+
+
+OPERATION_MAP={
+        "move": _move_device 
+}
+
+#
+# main handler for incoming messages
+# parse into json
+# call operation function with options
+async def _handle(websocket, path):
     async for message in websocket:
         parsed = json.loads(message)
-        dev = parsed["device"]
-        pan = parsed["inputSettings"]["pan"]
-        tilt = parsed["inputSettings"]["tilt"]
-        zoom = parsed["inputSettings"]["zoom"]
-        print(f"Setting dev {dev} pan: {pan} | tilt: {tilt} | zoom: {zoom}")
-        # validate dev/pan/tilt/zoom values
-        command = f"v4l2-ctl -c pan_absolute={pan},tilt_absolute={tilt},zoom_absolute={zoom} -d {dev}"
-        print(os.system(command))
-        #websocket.send(message)
+        operation = parsed["operation"]
+        options = parsed["options"]
+        func_to_call = OPERATION_MAP[operation.lower()]
+        if not func_to_call:
+            websocket.send("ERROR")
+            continue
+        websocket.send(func_to_call(options))
 
+    
 async def main():
-    async with websockets.serve(echo, "0.0.0.0", 8765):
+    async with websockets.serve(_handle, "0.0.0.0", 8765):
         await asyncio.Future()  # run forever
 
 asyncio.run(main())
